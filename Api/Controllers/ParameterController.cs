37d1c797402bc2b0b4bd9df822ba3d861446e733
kurt <feeleyfire@gmail.com>
2022-05-23 12:14:17 -0400

    update for .net 6

--- Api/Controllers/ParameterController.cs
+++ Api/Controllers/ParameterController.cs
@@ -1,32 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Api.Controllers
+namespace Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ParameterController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class ParameterController : ControllerBase
+    private IConfiguration _configuration;
+
+    public ParameterController(IConfiguration configuration)
     {
-        private IConfiguration _configuration;
+        _configuration = configuration;
+    }
 
-        public ParameterController(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
+    [HttpGet]
+    public IActionResult GetParameter()
+    {
 
+        string parameter1 = _configuration["test-key"];
+        string parameter2 = _configuration["test-key2"];
+        string parameter3 = _configuration["test-key3"];
 
-        [HttpGet]
-        public IActionResult GetParameter()
-        {
-
-            string parameter1 = _configuration["test-key"];
-            string parameter2 = _configuration["test-key2"];
-            string parameter3 = _configuration["test-key3"];
-
-            return Ok(new List<string> { parameter1, parameter2, parameter3 });
-
-        }
+        return Ok(new List<string> { parameter1, parameter2, parameter3 });
 
     }
 }
